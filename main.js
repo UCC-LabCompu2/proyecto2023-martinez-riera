63d62ea6611ca7fed73fdb92785de885fcd4a577
Rous444 <118020966+Rous444@users.noreply.github.com>
2023-06-05 23:43:01 -0300

    say

--- main.js
+++ main.js
@@ -1,25 +1,3 @@
-
-var contenedorImagen = document.getElementById('stock');
-var imagen = contenedorImagen.getElementsByTagName('img')[0];
-
-contenedorImagen.addEventListener('mouseover', function() {
-    imagen.classList.add('hidden');
-});
-
-contenedorImagen.addEventListener('mouseout', function() {
-    imagen.classList.remove('hidden');
-});
-contenedorImagen.addEventListener('mouseout', function() {
-    imagen.remove('img');
-});
-function convertirDolaresAPesos(dolares) {
-    // Tasa de cambio: 1 dólar = 100 pesos
-    var tasaCambio = 100;
-
-    var pesos = dolares * tasaCambio;
-
-    return pesos;
-}
 let input_val = document.getElementById('input_val');
 let num1 = document.getElementById('num1');
 let num2 = document.getElementById('num2');
@@ -30,16 +8,18 @@ num1.textContent = Math.floor(Math.random() * 10).toString();
 num2.textContent = Math.floor(Math.random() * 10).toString();
 let number1 = parseInt(num1.innerText, 10);
 let number2 = parseInt(num2.innerText, 10);
+
 myBtn.addEventListener("click", () => {
-    let sum_result = parseInt(number1) + parseInt(number2);
+    let sum_result = number1 + number2;
     let res = parseInt(input_val.value, 10);
 
     if (res === sum_result) {
-        alert("correcto");
+        alert("¡Correcto!");
+        window.location.href = "index.html";
     } else {
-        alert("incorrecto");
+        alert("¡Incorrecto!");
+        window.location.href = "perfil.html";
     }
-});
-
 
     window.location.reload();
+});

93c8c201f59d42d3e001f13b810c926918966380
Rous444 <118020966+Rous444@users.noreply.github.com>
2023-06-05 23:15:12 -0300

    asd

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+
+var contenedorImagen = document.getElementById('stock');
+var imagen = contenedorImagen.getElementsByTagName('img')[0];
+
+contenedorImagen.addEventListener('mouseover', function() {
+    imagen.classList.add('hidden');
+});
+
+contenedorImagen.addEventListener('mouseout', function() {
+    imagen.classList.remove('hidden');
+});
+contenedorImagen.addEventListener('mouseout', function() {
+    imagen.remove('img');
+});
 function convertirDolaresAPesos(dolares) {
     // Tasa de cambio: 1 dólar = 100 pesos
     var tasaCambio = 100;
@@ -12,21 +26,20 @@ let num2 = document.getElementById('num2');
 
 const myBtn = document.getElementById('btn');
 
-//
-num1.innerText = Math.floor(Math.random() * 10);
-num2.innerText = Math.floor(Math.random() * 10);
-let number1 = num1.innerText;
-let number2 = num2.innerText;
-
-myBtn.addEvenListener ("click",()=> {
+num1.textContent = Math.floor(Math.random() * 10).toString();
+num2.textContent = Math.floor(Math.random() * 10).toString();
+let number1 = parseInt(num1.innerText, 10);
+let number2 = parseInt(num2.innerText, 10);
+myBtn.addEventListener("click", () => {
     let sum_result = parseInt(number1) + parseInt(number2);
-    let res = parseInt(input_val.value)
+    let res = parseInt(input_val.value, 10);
 
-    if (res == sum_result) {
+    if (res === sum_result) {
         alert("correcto");
     } else {
         alert("incorrecto");
     }
+});
+
 
     window.location.reload();
-})

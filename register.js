9a5f6d77bd32debd1a880a76ad28ae414c328112
pepitomorzila <pepitomorzila@gmail.com>
2023-11-23 19:35:10 -0300

    Ultimas correcciones

--- register.js
+++ register.js
@@ -1,8 +1,14 @@
-document.getElementById("formulario").addEventListener("submit", function(event) {
+function validarContraseñas(event) {
     let password = document.getElementById("password").value;
     let confirm_password = document.getElementById("confirm_password").value;
     if (password !== confirm_password) {
         alert("Las contraseñas no coinciden");
         event.preventDefault();
     }
-});
+    else{
+        alert("Serás redireccionado a la página principal")
+    }
+}
+
+document.getElementById("formulario").addEventListener("submit", validarContraseñas);
+

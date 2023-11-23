427ebaa2aec313a3aaf6be6ac3b823171bb00a93
pepitomorzila <pepitomorzila@gmail.com>
2023-11-23 19:57:50 -0300

    Se comenta la funcion validarContrasenias

--- register.js
+++ register.js
@@ -1,4 +1,8 @@
-function validarContraseñas(event) {
+/**
+ * Esta función chequea que la contraseña introducida y el confirmar la contraseña introducida sean iguales. Caso que suceda, el usuario será redireccionado a la página principal (ya se registró), caso contrario se le informará mediante un alert de que las contraseñas no coinciden.
+ * @method validarContrasenias
+ */
+function validarContrasenias(event) {
     let password = document.getElementById("password").value;
     let confirm_password = document.getElementById("confirm_password").value;
     if (password !== confirm_password) {
@@ -9,6 +13,5 @@ function validarContraseñas(event) {
         alert("Serás redireccionado a la página principal")
     }
 }
-
-document.getElementById("formulario").addEventListener("submit", validarContraseñas);
+document.getElementById("formulario").addEventListener("submit", validarContrasenias);
 
